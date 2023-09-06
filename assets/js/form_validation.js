77171f97709ec9633f193b44145b7610148054c6
krishnaprasad45 <54229999+krishnaprasad45@users.noreply.github.com>
2023-09-07 00:24:18 +0530

    validation updated form_validation.js

--- assets/js/form_validation.js
+++ assets/js/form_validation.js
@@ -1,100 +1,87 @@
 function validation() {
-  var name = document.getElementById('name').value
-  var email = document.getElementById('email').value
-  var phone = document.getElementById('phone').value
-
-  var message = document.getElementById('m').value
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var phone = document.getElementById('phone').value;
+  var message = document.getElementById('message').value;
   var nameValidation = /^[A-Za-z]+$/;
 
+  var errorContainer = document.getElementById('error-container');
+  errorContainer.innerHTML = ''; // Clear previous error messages
 
-  if (name == '' && email == '' && phone == '' && message == '') {
-
-    alert("Enter all fields");
-    return false
-
+  if (name === '' || email === '' || phone === '' || message === '') {
+      showError("All fields are required");
+      return false;
   }
 
-  if (name == '') {
-    alert("Enter  name");
-    return false
-  } else if (name.indexOf(" ") >= 0) {
-    alert("Name not entered.")
-    return false
-  } else if (!nameValidation.test(name)) {
-    alert("Invalid characters");
-    return false
-  }
-  else if (name.length <= 3) {
-    alert("The  name must be atleast 3 character")
-    return false
+  if (name.indexOf(" ") >= 0) {
+      showError("Name should not contain spaces");
+      return false;
   }
 
-  else {
-    document.getElementById('name').innerHTML = ""
-    name = true
+  if (!nameValidation.test(name)) {
+      showError("Invalid characters in name");
+      return false;
   }
 
-
-  if (email == '') {
-    alert("Please enter your  email")
-    return false
-  } else if (email.length < 10) {
-    alert("Please enter  your correct E-mail")
-    return false
-  }
-  else {
-    document.getElementById('email').innerHTML = ""
-    email = true
+  if (name.length < 3) {
+      showError("Name must be at least 3 characters");
+      return false;
   }
 
-
-  if (phone == '') {
-    alert("Mobile number must not be Blank")
-    return false
-  } else if (phone.length != 10) {
-    alert("Enter valid mobile number")
-    return false
-  }
-  else {
-    document.getElementById('phone').innerHTML = ""
-    message = true
+  if (email.length < 10 || !isValidEmail(email)) {
+      showError("Please enter a valid email address");
+      return false;
   }
 
-
-  if (message == '') {
-    alert("Message must not be Blank")
-    return false
-  } else if (message.length < 5) {
-    alert("Message must be more than 5  char")
-    return false
-  }
-  else {
-    document.getElementById('message').innerHTML = ""
-    message = true
+  if (phone.length !== 10 || !isValidPhone(phone)) {
+      showError("Please enter a valid 10-digit phone number");
+      return false;
   }
 
+  if (message.length < 5) {
+      showError("Message must be at least 5 characters");
+      return false;
+  }
 
+  // No validation errors, clear the error container
+  errorContainer.innerHTML = '';
+  return true;
 }
 
+function showError(message) {
+  var errorContainer = document.getElementById('error-container');
+  var errorParagraph = document.createElement('p');
+  errorParagraph.className = 'error-message';
+  errorParagraph.textContent = message;
+  errorContainer.appendChild(errorParagraph);
+}
 
+function isValidEmail(email) {
+  // Add your email validation logic here (e.g., regex)
+  return true; // Replace with your actual validation
+}
 
+function isValidPhone(phone) {
+  // Add your phone validation logic here (e.g., regex)
+  return true; // Replace with your actual validation
+}
 
-
-
-$("#submit-form").submit((e) => {
-  e.preventDefault()
-  $.ajax({
-    url: "https://script.google.com/macros/s/AKfycbySyp0A7muiS75f_41H6joI6xIWt7U6dsW7P--r4ySpI3d2gcf0UZ4RpfDjZ-VIDPAw/exec",
-    data: $("#submit-form").serialize(),
-    method: "post",
-    success: function (response) {
-      alert("Form submitted successfully")
-      window.location.reload()
-      //window.location.href="https://google.com"
-    },
-    error: function (err) {
-      alert("Something Error")
-
-    }
-  })
-})
+$(document).ready(function () {
+  $("#submit-form").submit(function (e) {
+      e.preventDefault();
+      if (validation()) {
+          $.ajax({
+              url: "https://script.google.com/macros/s/AKfycbySyp0A7muiS75f_41H6joI6xIWt7U6dsW7P--r4ySpI3d2gcf0UZ4RpfDjZ-VIDPAw/exec",
+              data: $("#submit-form").serialize(),
+              method: "post",
+              success: function (response) {
+                  alert("Form submitted successfully");
+                  window.location.reload();
+              },
+              error: function (err) {
+                  alert("Something went wrong");
+              }
+          });
+      }
+  });
+});
